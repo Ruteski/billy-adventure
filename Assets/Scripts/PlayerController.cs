81bf298aae1966d9eed869f94d8aed227b0e5b47
Lincoln Ruteski <lruteski@gmail.com>
2022-04-27 14:13:12 -0300

    fazendo o player pular

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
 
     [Header("Movimentacao")]
-    [SerializeField] private float velocidade = 5f;
+    [SerializeField] private float velh = 5f;
+    [SerializeField] private float velv = 5f;
     private Rigidbody2D meuRB;
     private SpriteRenderer sprite;
     private Animator meuAnim;
@@ -21,7 +22,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        
+        Pulando();
     }
 
     private void FixedUpdate() {
@@ -29,7 +30,7 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Movimento() {
-        var horizontal = Input.GetAxis("Horizontal") * velocidade;
+        var horizontal = Input.GetAxis("Horizontal") * velh;
         Vector2 minhaVelocidade = new Vector2(horizontal, meuRB.velocity.y);
 
         meuRB.velocity = minhaVelocidade;
@@ -46,10 +47,20 @@ public class PlayerController : MonoBehaviour
             meuAnim.SetBool("Movendo", false);
         }
 
+
+
         //tb poderia ter sido feito assim
         //transform.localScale = new Vector3(Mathf.Sign(horizontal), 1f, 1f);
 
         //para mudar a transicao da animacao tb poderia fazer assim
         //meuAnim.SetBool("Movendo", horizontal != 0);    
+    }
+
+    private void Pulando() {
+         var pulo = Input.GetButtonDown("Jump");
+
+        if (pulo) {    
+            meuRB.velocity = new Vector2(meuRB.velocity.x, velv);   
+        }
     }
 }
