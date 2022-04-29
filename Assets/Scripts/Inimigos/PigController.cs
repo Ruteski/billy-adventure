f5affdd2542412d92b4662cd11ac4746262b04bb
Lincoln Ruteski <lruteski@gmail.com>
2022-04-29 19:26:29 -0300

    Fazendo o inimigo ter um timer e um método para a movimentação

--- Assets/Scripts/Inimigos/PigController.cs
+++ Assets/Scripts/Inimigos/PigController.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class PigController : MonoBehaviour
 {
     [SerializeField] private float velh = 2f;
+    [SerializeField] private float esperaMovimento = 1f;
     private Rigidbody2D meuRB;
+    
     
     // Start is called before the first frame update
     private void Start() {
         meuRB = GetComponent<Rigidbody2D>();
-        meuRB.velocity = new Vector2(velh, meuRB.velocity.y);
-
-        transform.localScale = new Vector3(Mathf.Sign(meuRB.velocity.x) * -1, 1f, 1f);
     }
 
     // Update is called once per frame
     private void Update() {
-        
+        Move();
+    }
+
+    private void Move() {
+        if (esperaMovimento <= 0) {
+            //lado de movimentacao aleatorio
+            float _velh = velh * Mathf.Sign(Random.Range(-1, 1));
+
+            meuRB.velocity = new Vector2(_velh, meuRB.velocity.y);
+            transform.localScale = new Vector3(Mathf.Sign(meuRB.velocity.x) * -1, 1f, 1f);
+
+            esperaMovimento = Random.Range(1f, 3f);
+        } else {
+            esperaMovimento -= Time.deltaTime;
+        }
     }
 }
