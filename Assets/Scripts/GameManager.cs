f0956329673dd74c4d8509500b3011c2360ba5a7
Lincoln Ruteski <lruteski@gmail.com>
2022-05-01 17:18:53 -0300

    criando no gamemanager a variavel vetor para guardar as imagens dos corações

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Image[] coracoes;
+    
     private static int vida = 3;
     private int vidaInicial = 3;
+
 
     // Start is called before the first frame update
     void Start()
